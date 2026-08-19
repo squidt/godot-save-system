19213f623662fa961afec17cd06dc8e7dc21bfc7
squidt <106044562+squidt@users.noreply.github.com>
2026-08-19 14:30:00 -0600

    feat: add `save_manager`

---
+++ addons/save-system/core/manager.gd
@@ -0,0 +1,110 @@
+class_name SaveManager extends Saveable
+
+const VERSION := &"0.0.1a"
+const USER = &"user://"
+const FOLDER = &"saves/"
+const EXTENSION = &".json"
+
+# TODO:
+# profiles -> name, newest save timestamp, newest save thumbnail
+# slot -> name, timestamp, thumbnail
+
+static var save_name = &"profile1"
+static var this
+
+
+static func make_filepath(save_name: StringName) -> StringName:
+	return USER + FOLDER + save_name + EXTENSION
+
+
+static func _write_file(save_name: StringName, data: Dictionary) -> void:
+	var dir := DirAccess.open(USER)
+	if !dir.dir_exists(FOLDER):
+		dir.make_dir_recursive(FOLDER)
+
+	var filepath = make_filepath(save_name)
+	var file := FileAccess.open(filepath, FileAccess.WRITE)
+	if !file:
+		print("FileAccess Error: ", error_string(FileAccess.get_open_error()))
+	var json := JSON.stringify(data, "\t", false)
+	file.store_string(json)
+	file.close()
+
+
+static func _read_file(save_name: StringName) -> Dictionary:
+	var filepath = make_filepath(save_name)
+	if !FileAccess.file_exists(filepath):
+		printerr("File (%s) does not exist" % [filepath])
+		return {}
+
+	var file := FileAccess.open(filepath, FileAccess.READ)
+	var data := JSON.parse_string(file.get_as_text()) as Dictionary
+	file.close()
+	return data
+
+
+func _ready() -> void:
+	_recreated = false
+	this = self
+	super()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Engine.is_editor_hint():
+		return
+
+	if event.is_action_pressed(&"save_game"):
+		save_game()
+	elif event.is_action_pressed(&"load_game"):
+		load_game()
+	elif event.is_action_pressed(&"print_save"):
+		_debug_print_serialization()
+
+
+func is_save_owner() -> bool:
+	return true
+
+
+static func save_game() -> void:
+	_write_file(this.save_name, this.serialized())
+
+
+static func load_game() -> void:
+	var data := _read_file(save_name)
+
+	if !data.has("name") or !data.has("date") or !data.has("version") or !data.has("game"):
+		printerr("Expected required fields in game save")
+		return
+
+	this.deserialize(data)
+
+
+func serialized() -> Dictionary:
+	saving.emit()
+
+	var node := get_parent()
+	var data := {
+		"name": save_name,
+		"date": Time.get_datetime_string_from_system(),
+		"version": VERSION,
+		"state": SaveStep.Ownership.to(node)
+	}
+
+	saved.emit()
+	return data
+
+
+func deserialize(data: Dictionary) -> void:
+	loading.emit()
+
+	SaveStep.Ownership.from(get_parent(), data.get("game"))
+
+	loaded.emit()
+
+#static func _queue_free_first_saveables(node: Node) -> void:
+#for child in node.get_children():
+#var saveable = Saveable.as_trait(child) if Saveable.is_trait(child) else null
+#if saveable and saveable.is_save_owner():
+#child.queue_free()
+#else:
+#_queue_free_first_saveables(child)
